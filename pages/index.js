2a02df2b54ee1a5d2f30e61ca79678924ed85257
Jarrell Ball <brianbll9@gmail.com>
2022-07-14 13:51:42 -0400

    fix import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { AudioPlayer } from "./components/AudioPlayer";
+import AudioPlayer from "./components/AudioPlayer";
 export default function Home() {
   return (
     <div className={styles.container}>
