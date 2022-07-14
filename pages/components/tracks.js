fcb293f1f3a8d10285662b54988596eb9f24f382
Jarrell Ball <jarrellball@Jarrells-MacBook-Pro.local>
2022-07-14 08:08:09 -0400

    update player

---
+++ pages/components/tracks.js
@@ -0,0 +1,39 @@
+
+import deep from '../assets/deep.mp3';
+import war from '../assets/war.mp3';
+import imgSrc from '../assets/bigwin-logo.png'
+import toliver from '../assets/toliver.jpeg'
+import mindsetCover from '../assets/mindset.png'
+import mindset from '../assets/mindset1.mp3';
+
+
+
+export default [
+  {
+    title: "Mindset",
+    artist: "Carol Dweck",
+    audioSrc: mindset,
+    chapter: 'Chapter 1',
+    coverArt: mindsetCover,
+  },
+  {
+    title: "War",
+    artist: "Jarrell Brain",
+    audioSrc: war,
+    coverArt: imgSrc,
+  },
+  {
+    title: "50",
+    artist: "Don Toliver",
+    audioSrc: deep,
+    coverArt: toliver,
+  },
+  {
+    title: "50",
+    artist: "Something",
+    audioSrc: 'https://mailboxdrive.com/mp3s/20339/#:~:text=Direct%20Link%3A-,here,-Play%20Time%3A%201',
+    coverArt: deep,
+  },
+];
+// All of these artists are at https://pixabay.com/music/search/mood/laid%20back/
+
